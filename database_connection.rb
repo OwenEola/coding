89845a2967eeafb196000ea1b597c46d1046e1fc
Owen Davies <owen@eola.co>
2023-05-09 18:24:59 +0100

    local database

--- database_connection.rb
+++ database_connection.rb
@@ -2,12 +2,13 @@ require 'sqlite3'
 
 class DatabaseConnection
   def initialize
-    @db = SQLite3::Database.new ":memory:"
+    @db = SQLite3::Database.new "my_database.db"
 
     # Create the players table if it doesn't exist
     @db.execute <<-SQL
       create table if not exists players (
         id INTEGER PRIMARY KEY,
+        created_at datetime default current_timestamp,
         name varchar(50),
         score int(5)
       );
